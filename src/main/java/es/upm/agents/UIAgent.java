734056c41500c541b8497b782c46f2e07c1ef0f0
Santiago <santiagorodriguezgsm@gmail.com>
2026-05-22 01:12:33 +0200

    Update UIAgent.java

--- src/main/java/es/upm/agents/UIAgent.java
+++ src/main/java/es/upm/agents/UIAgent.java
@@ -1,4 +1,48 @@
 package es.upm.agents;
 
-public class UIAgent {
+import es.upm.idvehiculos.AgentBase;
+import es.upm.idvehiculos.AgentModel;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class UIAgent extends AgentBase {
+
+    public static final String NICKNAME = "UIAgent";
+
+    @Override
+    protected void setup() {
+        this.type = AgentModel.UI;
+        super.setup();
+        log("Iniciado");
+        registerAgentDF();
+        addBehaviour(new DetectionResultReceiverBehaviour());
+    }
+
+    private class DetectionResultReceiverBehaviour extends CyclicBehaviour {
+        @Override
+        public void action() {
+            MessageTemplate mt = MessageTemplate.and(
+                    MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                    MessageTemplate.MatchOntology("detection-result")
+            );
+
+            ACLMessage msg = myAgent.receive(mt);
+
+            if (msg != null) {
+                String result = msg.getContent();
+                log("Resultado de detección recibido: " + result);
+
+                // Aquí podrías actualizar una GUI, enviar a web, etc.
+            } else {
+                block();
+            }
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        deregisterAgentDF();
+        log("Terminando UIAgent...");
+    }
 }
